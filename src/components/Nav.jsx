6a21d202095086addadf461f8fc92e2de144b553
Zhir Bakhtyar <zheerbaxtyar9@gmail.com>
2026-02-07 16:35:55 +0300

    making some improvements to the navbar and figuring out which font to use

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Nav = () => {
   return (
     <nav>
-      
+      <div className="nav-content">
+        <h3>
+          Winstrxl
+        </h3>
+      </div>
     </nav>
   )
 }
